4dc34a89a190c7e40be79104171f44894b0e9b10
VictorTumbiolo <115381451+VictorTumbiolo@users.noreply.github.com>
2026-09-18 12:45:32 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
+import './styles/AboutPage.css'
+import './styles/AppShell.css'
+import './styles/ChatPanel.css'
+import './styles/PapersSidebar.css'
+import './styles/PaperViewer.css'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
